55657d229d3cbedd07a9ec3ca3cd1075fa4efb3e
Joshua Olds <jolds@bastiansolutions.com>
2017-07-07 21:26:49 -0600

    Release Candidate

---
+++ poses.h
@@ -0,0 +1,44 @@
+#ifndef POSES
+#define POSES
+
+#include <avr/pgmspace.h>
+
+PROGMEM prog_uint16_t CENTER[] = {5, 512, 512, 512, 512, 874};
+PROGMEM prog_uint16_t HOME[] = {5, 506, 346, 871, 743, 874};
+
+PROGMEM prog_uint16_t YEL_PERCH[] = {5, 430, 631, 675, 713, 874};
+PROGMEM prog_uint16_t YEL_PRESS[] = {5, 428, 700, 710, 720, 874};
+
+PROGMEM prog_uint16_t WHT_PERCH[] = {5, 475, 537, 803, 711, 874};
+PROGMEM prog_uint16_t WHT_PRESS[] = {5, 474, 600, 830, 710, 874};
+
+PROGMEM prog_uint16_t RED_PERCH[] = {5, 547, 620, 680, 741, 874};
+PROGMEM prog_uint16_t RED_PRESS[] = {5, 544, 690, 710, 737, 874};
+
+PROGMEM prog_uint16_t GRN_PERCH[] = {5, 625, 580, 746, 737, 874};
+PROGMEM prog_uint16_t GRN_PRESS[] = {5, 625, 644, 767, 742, 874};
+
+PROGMEM prog_uint16_t ARM_WIN_1[] = {5, 512, 512, 512, 512, 874};
+PROGMEM prog_uint16_t ARM_WIN_2[] = {5, 750, 512, 512, 512, 874};
+PROGMEM prog_uint16_t ARM_WIN_3[] = {5, 750, 512, 750, 512, 874};
+PROGMEM prog_uint16_t ARM_WIN_4[] = {5, 750, 512, 512, 512, 874};
+PROGMEM prog_uint16_t ARM_WIN_5[] = {5, 250, 512, 512, 512, 874};
+PROGMEM prog_uint16_t ARM_WIN_6[] = {5, 250, 512, 750, 512, 874};
+PROGMEM prog_uint16_t ARM_WIN_7[] = {5, 250, 512, 512, 512, 874};
+
+PROGMEM prog_uint16_t ARM_LOSE_1[] = {5, 506, 346, 871, 743, 874};
+PROGMEM prog_uint16_t ARM_LOSE_2[] = {5, 506, 346, 950, 743, 874};
+PROGMEM prog_uint16_t ARM_LOSE_3[] = {5, 250, 346, 950, 743, 874};
+PROGMEM prog_uint16_t ARM_LOSE_4[] = {5, 750, 346, 950, 743, 874};
+PROGMEM prog_uint16_t ARM_LOSE_5[] = {5, 250, 346, 950, 743, 874};
+PROGMEM prog_uint16_t ARM_LOSE_6[] = {5, 506, 346, 871, 743, 874};
+
+PROGMEM prog_uint16_t GRN_MOCK_1[] = {5, 625, 580, 746, 737, 874};
+PROGMEM prog_uint16_t GRN_MOCK_2[] = {5, 650, 580, 746, 737, 874};
+PROGMEM prog_uint16_t GRN_MOCK_3[] = {5, 600, 580, 746, 737, 874};
+PROGMEM prog_uint16_t GRN_MOCK_4[] = {5, 650, 580, 746, 737, 874};
+PROGMEM prog_uint16_t GRN_MOCK_5[] = {5, 600, 580, 746, 737, 874};
+
+
+
+#endif
